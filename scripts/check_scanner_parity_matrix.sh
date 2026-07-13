76d94f7d6a03326e60d119552db0b461e11b7aee
Michel Blomgren <mike@pkt.systems>
2026-07-13 02:31:02 +0200

    test(scanner): cover large source mutation parity

--- scripts/check_scanner_parity_matrix.sh
+++ scripts/check_scanner_parity_matrix.sh
@@ -145,5 +145,7 @@ scanner_parity_run_row build/direct-probe/lockd-100k.ndjson lockd_100k \
   lockd_write_event '/op="write"' decision_only_selector /id
 scanner_parity_run_row build/direct-probe/large-4x25m.ndjson large_4x25m \
   eq_status_open_top_set '/status="open"' mutate_file_selector /id
+scanner_parity_run_row build/direct-probe/large-4x25m.ndjson large_4x25m \
+  eq_status_open_top_set '/status="open"' mutate_source_selector /id
 
 scanner_parity_validate
