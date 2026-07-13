9f0494e1d45764218537c5cf03443a33b041985c
Michel Blomgren <mike@pkt.systems>
2026-07-13 02:40:46 +0200

    test(scanner): cover large source projection parity

--- scripts/check_scanner_parity_matrix.sh
+++ scripts/check_scanner_parity_matrix.sh
@@ -147,5 +147,7 @@ scanner_parity_run_row build/direct-probe/large-4x25m.ndjson large_4x25m \
   eq_status_open_top_set '/status="open"' mutate_file_selector /id
 scanner_parity_run_row build/direct-probe/large-4x25m.ndjson large_4x25m \
   eq_status_open_top_set '/status="open"' mutate_source_selector /id
+scanner_parity_run_row build/direct-probe/large-4x25m.ndjson large_4x25m \
+  eq_status_open '/status="open"' project_source_selector /id
 
 scanner_parity_validate
